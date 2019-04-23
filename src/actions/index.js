001f595dd74c263500b3b69e517090720958e56a
theBitCoiner <thebitcoinerapps@gmail.com>
2019-04-23 13:32:58 +0200

    Reducers created

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,8 @@ import jsonPlaceholder from '../apis/JSONplaceholder';
 
 export const fetchPost = () => {
 
-    return async function(dispatch, getState) {
-        const res = await jsonPlaceholder.get('/post');
+    return async (dispatch) => {
+        const res = await jsonPlaceholder.get('/posts');
 
         dispatch({type: 'FETCH_POST', payload: res});
     }
