629b148871b09806eefdbb311c93bb367597773b
theBitCoiner <thebitcoinerapps@gmail.com>
2019-04-24 13:41:51 +0200

    small refactors added

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import UserHeader from './UserHeader';
 
 import { fetchPost } from '../actions';
 
@@ -18,6 +19,7 @@ class PostList extends React.Component{
                         <h2>{post.title}</h2>
                         <p>{post.body}</p>
                         </div>
+                        <UserHeader userId={post.userId} />
                     </div>
                 </div>
             );
